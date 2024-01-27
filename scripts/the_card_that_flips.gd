9bb9736354903c5db1a232fea22a88412b914368
soko7awen <havenhamelin@gmail.com>
2024-01-27 16:28:05 -0500

    Update the_card_that_flips.gd

--- scripts/the_card_that_flips.gd
+++ scripts/the_card_that_flips.gd
@@ -3,7 +3,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var main = get_tree().root.get_child(0)
-	print(main.players)
+	print(main.players.pick_random())
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
