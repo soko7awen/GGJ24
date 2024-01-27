1ac5239c4b894e48c994b93410c27b548a5a5692
zdicken <zdicken@iu.edu>
2024-01-27 16:07:55 -0500

    so you didn't do it right

---
+++ scripts/card_select.gd
@@ -0,0 +1,8 @@
+extends Node2D
+@onready var gameController = $".."
+var nextMinigame = randi_range(1,5)
+
+func _ready():
+	while(nextMinigame != gameController.lastMinigame):
+		nextMinigame = randi_range(1,5)
+	
