45966b4b33eb4d82a68036db240758ebc40951da
soko7awen <havenhamelin@gmail.com>
2024-01-27 18:39:57 -0500

    gimme gimme

--- scripts/the_card_that_flips.gd
+++ scripts/the_card_that_flips.gd
@@ -1,14 +1,14 @@
 extends Node2D
 @onready var gameController = $".."
 var toScene = "res://scenes/cutscenes/card_select.tscn"
-var colorCipher = [Color8(255,0,77),Color8(41,173,255),Color8(0,228,54),Color8(0,236,39),Color8(255,163,0),Color8(255,119,168)]
+var colors = gameController.colorCipher
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var main = get_tree().root.get_child(0)
 	main.pickKing()
 	$CanvasLayer/Label.text = main.king[1]
-	$CanvasLayer/Label.set("theme_override_colors/font_color",colorCipher[main.king[0]])
+	$CanvasLayer/Label.set("theme_override_colors/font_color",colors[main.king[0]])
 	#main.loadScene(self,toScene)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
