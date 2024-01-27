3ade164388d5ce0941e15bed084c3ba13ff5f459
soko7awen <havenhamelin@gmail.com>
2024-01-27 14:24:32 -0500

    yey

--- scripts/player_select.gd
+++ scripts/player_select.gd
@@ -1,10 +1,11 @@
 extends Node2D
 @onready var gameController = $".."
 @onready var players = get_node("UI/Players")
+var toScene = "res://scenes/cutscenes/the_card_that_flips.tscn"
 var player = 1
 
 func _process(delta):
 	pass
 
 func _on_start_button_pressed():
-	pass
+	$"..".loadScene(self,toScene)
