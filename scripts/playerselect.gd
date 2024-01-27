9dc68e22d55aabdaaa432e9f7d85a713905b8296
zdicken <zdicken@iu.edu>
2024-01-27 13:46:40 -0500

    New !

--- scripts/playerselect.gd
+++ scripts/playerselect.gd
@@ -1,17 +1,10 @@
 extends Node2D
-@onready var gameController = get_tree().root.get_node("main")
-@onready var input = get_tree().root.get_node("main/Playerselect/UI/PlayerInput")
-@onready var text = get_tree().root.get_node("main/Playerselect/UI/PlayerText")
+@onready var gameController = $".."
+@onready var players = get_node("UI/Players")
 var player = 1
 
 func _process(delta):
 	pass
 
 func _on_next_button_pressed():
-	if(input.text.length() > 0):
-		gameController.players.append(input.text)
-		input.text = ""
-		player += 1
-		text.text = "Input Player " + str(player) + " Name"
-	else:
-		pass #handle no input
+	pass
