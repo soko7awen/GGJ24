c50bae150f4cbaaeb586fe04340c3ceef3995f9c
zdicken <zdicken@iu.edu>
2024-01-27 11:28:32 -0500

    basic player select

---
+++ scenes/playerselect.gd
@@ -0,0 +1,17 @@
+extends Node2D
+var gameController = get_tree().root.get_node("main")
+@onready var input = get_tree().root.get_node("main/Playerselect/UI/PlayerInput")
+@onready var text = get_tree().root.get_node("main/Playerselect/UI/PlayerText")
+var player = 1
+
+func _process(delta):
+	pass
+
+func _on_next_button_pressed():
+	if(input.text.length() > 0):
+		gameController.players.append(input.text)
+		input.text = ""
+		player += 1
+		text.text = "Input Player " + player + " Name"
+	else:
+		pass #handle no input
--- scripts/SceneLoader.gd
+++ scripts/SceneLoader.gd
@@ -1,8 +1,6 @@
 extends Node2D
-
-
-func _ready():
-	pass # Replace with function body.
+var players = []
 
 func _process(delta):
 	pass
+
