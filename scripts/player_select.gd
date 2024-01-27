94c359ede67fa8246f7eda9124b4ea69eeb17f51
zdicken <zdicken@iu.edu>
2024-01-27 14:41:12 -0500

    side to side button works

--- scripts/player_select.gd
+++ scripts/player_select.gd
@@ -1,6 +1,7 @@
 extends Node2D
 @onready var gameController = $".."
 @onready var players = get_node("UI/Players")
+@onready var camera = get_node("Camera")
 var toScene = "res://scenes/cutscenes/the_card_that_flips.tscn"
 var player = 1
 
@@ -8,7 +9,11 @@ func _process(delta):
 	pass
 
 func _on_right_button_pressed():
-	pass
+	camera.position.x += 1152
+	
+func _on_left_button_pressed():
+	camera.position.x -= 1152
 
 func _on_start_button_pressed():
 	$"..".loadScene(self,toScene)
+
