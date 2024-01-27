96b18525e1b487f133257729e86dce47bf940a67
soko7awen <havenhamelin@gmail.com>
2024-01-27 18:34:51 -0500

    wee!

--- scripts/scene_loader.gd
+++ scripts/scene_loader.gd
@@ -1,8 +1,12 @@
 extends Node2D
 var players = []
+var king = 0
 var lastMinigame = 5
 
 func loadScene(old_scene,scene_path):
 	var newScene = load(scene_path).instantiate()
 	old_scene.queue_free()
 	add_child(newScene)
+
+func pickKing():
+	king = players.pick_random()
