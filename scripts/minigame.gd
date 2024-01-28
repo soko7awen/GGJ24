b1d521d9046b808f981ffd63909e4444671bb151
zdicken <zdicken@iu.edu>
2024-01-27 20:06:05 -0500

    Consent is Here!

--- scripts/minigame.gd
+++ scripts/minigame.gd
@@ -17,8 +17,9 @@ func _on_start_button_pressed():
 func _on_finish_button_pressed():
 	$"../Camera2D".position.y -= 972
 	player += 1
+	checkIfKing()
 	setTransition(player)
 
 func checkIfKing():
-	if(player == gameController.king):
+	if(player == gameController.king[0]):
 		player += 1
--- scripts/scene_loader.gd
+++ scripts/scene_loader.gd
@@ -1,7 +1,7 @@
 extends Node2D
 var players = []
 var lastMinigame = null
-var king = 0
+var king = null
 var colorCipher = [Color8(255,0,77),Color8(41,173,255),Color8(0,228,54),Color8(255,236,39),Color8(255,163,0),Color8(255,119,168)]
 
 func loadScene(old_scene,scene_path):
@@ -9,5 +9,5 @@ func loadScene(old_scene,scene_path):
 	old_scene.queue_free()
 	add_child(newScene)
 
-func pickKing():
+func pickKing(): #picking his nose lmao
 	king = players.pick_random()
