a0f60feb12c6a069ee91e08d3a98954f2fc867a9
zdicken <zdicken@iu.edu>
2024-01-28 14:27:07 -0500

    holy Shit Divine Intervention

--- scripts/the_bidding.gd
+++ scripts/the_bidding.gd
@@ -6,8 +6,12 @@ var minMax = null
 
 func _ready():
 	for i in $Jesters.get_child_count():
-		if(main.colorNameCipher[main.players[i]] == $Jesters.get_child(i).name and main.players[i][0] != main.king[0]):
-			$Jesters.get_child(i).visible == true
+		print(main.colorNameCipher[main.players[i][0]] + " =? " + $Jesters.get_child(i).name)
+		print(str(main.players[i][0]) + " =? " + str(main.king[0]))
+		for j in main.players.size() - 1:
+			if(main.colorNameCipher[main.players[i][0]] == $Jesters.get_child(i).name):
+				if main.players[i][0] != main.king[0]:
+					$Jesters.get_child(i).visible = true
 	$king.texture = load("res://assets/sprites/judgement/Jkings/Jking_"+main.colorNameCipher[main.king[0]]+".png")
 	#mixMax = [main.players[0]]
 	#lane = minMax[0]
