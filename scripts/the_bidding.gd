9f6a8d3afcad38dcdd43ed9c8296d7dd85893849
soko7awen <havenhamelin@gmail.com>
2024-01-28 14:43:11 -0500

    ewr

--- scripts/the_bidding.gd
+++ scripts/the_bidding.gd
@@ -5,13 +5,8 @@ var lane = null
 var minMax = null
 
 func _ready():
-	for i in $Jesters.get_child_count():
-		print(main.colorNameCipher[main.players[i][0]] + " =? " + $Jesters.get_child(i).name)
-		print(str(main.players[i][0]) + " =? " + str(main.king[0]))
-		for j in main.players.size() - 1:
-			if(main.colorNameCipher[main.players[i][0]] == $Jesters.get_child(i).name):
-				if main.players[i][0] != main.king[0]:
-					$Jesters.get_child(i).visible = true
+	for i in main.players:
+		$Jesters.get_child(i[0]).visible = true
 	$king.texture = load("res://assets/sprites/judgement/Jkings/Jking_"+main.colorNameCipher[main.king[0]]+".png")
 	#mixMax = [main.players[0]]
 	#lane = minMax[0]
