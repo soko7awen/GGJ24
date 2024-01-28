114d616d5e9532e1dc290da35f3cad731417d9ca
zdicken <zdicken@iu.edu>
2024-01-27 19:52:07 -0500

    holy FUCK

--- scripts/minigame.gd
+++ scripts/minigame.gd
@@ -1,12 +1,16 @@
 extends Node2D
-@onready var gameController = $".."
+@onready var gameController = $"../.."
 
 func _ready():
-	setTransition(gameController.players[0])
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	setTransition(0)
+	$"../BottomUI/Background".modulate = gameController.colorCipher[gameController.king[0]]
 
 func setTransition(nextPlayer):
-	$"../Background".color = gameController.colorCipher[nextPlayer]
+	$"../TopUI/Background".color = gameController.colorCipher[gameController.players[nextPlayer][0]]
+	$"../TopUI/nameText".text = "[center]" + gameController.players[nextPlayer][1] + "[/center]"
+
+func _on_start_button_pressed():
+	$Camera2D.position.y += 972
+
+func _on_finish_button_pressed():
+	pass
