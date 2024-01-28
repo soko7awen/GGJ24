aff69c69596f6f383da5224e520d7dfa389bebc4
soko7awen <havenhamelin@gmail.com>
2024-01-27 19:59:26 -0500

    commit die

--- scripts/scene_loader.gd
+++ scripts/scene_loader.gd
@@ -2,7 +2,7 @@ extends Node2D
 var players = []
 var lastMinigame = null
 var king = 0
-var colorCipher = [Color8(255,0,77),Color8(41,173,255),Color8(0,228,54),Color8(0,236,39),Color8(255,163,0),Color8(255,119,168)]
+var colorCipher = [Color8(255,0,77),Color8(41,173,255),Color8(0,228,54),Color8(255,236,39),Color8(255,163,0),Color8(255,119,168)]
 
 func loadScene(old_scene,scene_path):
 	var newScene = load(scene_path).instantiate()
