379225e1454ec3198df1d5422f6fd2593110facf
zdicken <zdicken@iu.edu>
2024-01-28 13:53:02 -0500

    Revert "Merge branch 'main' of https://github.com/soko7awen/GGJ24"
    This reverts commit 25614e1d2bfc305b67a4aed7acafcccc3f8d4fbc, reversing
    changes made to 07b57293ccfb222de32fd5b55cd5bc56751d243b.

--- scripts/the_card_that_flips.gd
+++ scripts/the_card_that_flips.gd
@@ -2,13 +2,15 @@ extends Node2D
 @onready var main = get_tree().root.get_child(0)
 var toScene = "res://scenes/cutscenes/card_select.tscn"
 @onready var colors = main.colorCipher
+@export var flipped = false
+var unflipped = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if main.king == null:
 		main.pickKing()
-	$Label.text = "[center]" + main.king[1] + "[/center]"
-	$Label.set("theme_override_colors/default_color",colors[main.king[0]])
+	$Label.text = main.king[1]
+	$Label.set("theme_override_colors/font_color",colors[main.king[0]])
 	$AnimationPlayer.play("Flip")
 
 func _on_next_button_pressed():
@@ -17,6 +19,5 @@ func _on_next_button_pressed():
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "Flip":
 		$TextureRect.texture = load("res://assets/sprites/kings/king_"+main.colorNameCipher[main.king[0]]+".png")
-		$Label.visible = true
 		$AnimationPlayer.play("Unflip")
 		
