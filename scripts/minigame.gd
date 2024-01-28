b11689b7829a3e5af2620a91ad376749551926e5
zdicken <zdicken@iu.edu>
2024-01-27 20:33:42 -0500

    push Push me of fa bridge

--- scripts/minigame.gd
+++ scripts/minigame.gd
@@ -1,6 +1,9 @@
 extends Node2D
 @onready var gameController = $"../.."
+@onready var input = $"../BottomUI/input"
 var player = 0
+var responses = []
+var toScene = "res://scenes/judgement.tscn"
 
 func _ready():
 	checkIfKing()
@@ -13,12 +16,20 @@ func setTransition(nextPlayer):
 
 func _on_start_button_pressed():
 	$"../Camera2D".position.y += 972
+	input.grab_focus()
 
 func _on_finish_button_pressed():
 	$"../Camera2D".position.y -= 972
-	player += 1
-	checkIfKing()
-	setTransition(player)
+	responses.append([int(player), input.text])
+	if((player + 1) > gameController.players.size()):
+		gameController.loadScene(self,toScene)
+	else:
+		player += 1
+		checkIfKing()
+		setTransition(player)
+		input.text = ""
+		input.grab_focus()
+		input.release_focus()
 
 func checkIfKing():
 	if(player == gameController.king[0]):
