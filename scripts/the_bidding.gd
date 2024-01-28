d1cca07fbe2672bc9ea450372b17cba60844a52b
soko7awen <havenhamelin@gmail.com>
2024-01-28 15:10:46 -0500

    nooooo weierrd

--- scripts/the_bidding.gd
+++ scripts/the_bidding.gd
@@ -1,6 +1,7 @@
 extends Node2D
-@onready var arrow = $ArrowLargeWhite
+@onready var arrow = $Arrow
 @onready var main = get_tree().root.get_child(0)
+var arrowPos = 0
 var lane = null
 var lanes = []
 
@@ -8,8 +9,16 @@ func _ready():
 	for i in main.players:
 		$Jesters.get_child(i[0]).visible = true
 	$king.texture = load("res://assets/sprites/judgement/Jkings/Jking_"+main.colorNameCipher[main.king[0]]+".png")
-	#haven PLEASE
 
 func _process(delta):
 	if Input.is_action_just_pressed("left"):
+		arrowPos -= 1
+	if Input.is_action_just_pressed("right"):
+		arrowPos += 1
+	if arrowPos >= main.players.size():
+		arrowPos = 0
+	arrow.position = $Jesters.get_child(arrowPos).get_child(0).position
+	if Input.is_action_just_pressed("submit"):
+		#main.score[player[0]] += 1
+		#main.king = main.players[player[0]]
 		pass
